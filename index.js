f54c9bf73aebc8378abb10bc5e74eff4448a02c3
Pitbullts <64328833+Pitbullts@users.noreply.github.com>
2021-10-21 23:50:53 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+const productos = [{id: 1, producto: "RTX 3050 4GB"},
+                    {id: 2, producto: "RTX 3060 12GB"},
+                    {id: 3, producto: "RTX 3060ti 8GB"},
+                    {id: 4, producto: "Core I3 10100"},
+                    {id: 5, producto: "Core I5 10400"},
+                    {id: 6, producto: "Core I7 10700"},]
